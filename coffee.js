444bfd7039b6d4f141d4948efc6fede45a410f83
Juha Ruokonen <juha.ruokonen@trimico.fi>
2018-05-29 18:40:50 +0300

    Added BBS-compatible graphics

--- coffee.js
+++ coffee.js
@@ -1,5 +1,31 @@
-console.log(isItTime() ? 'Yes, it is time!' : 'Not yet :(');
+if(isItTime()) {
+  coffeeSuccess();
+} else {
+  coffeeFailure();
+}
 
 function isItTime() {
   return Math.random() >= 0.9;
+}
+
+function coffeeSuccess() {
+    console.log(` 
+           {
+        {   }
+         }_{ __{
+      .-{   }   }-.
+     (   }     {   )
+     |\`-.._____..-'|
+     |             ;--.
+     |            (__  \\
+     |             | )  )
+     |             |/  /
+     |             /  /
+     |            (  /
+     \\             y'
+      \`-.._____..-'`);
+}
+
+function coffeeFailure() {
+  console.log('Not yet :(');
 }
